986510ff37e218f171a944fa1986b7f85d5138c8
Tilak <tilaksasmal999@gmail.com>
2025-06-16 16:03:40 +0000

    updated port and host

--- main.ts
+++ main.ts
@@ -5,7 +5,10 @@ import { server } from "./router"
 
 const main = async () => {
     const app = await server()
-    const address = app.listen({ port: 3000 })
+    const address = app.listen({
+        port: parseInt(process.env.PORT || "3000"),
+        host: process.env.HOST || "0.0.0.0"
+    })
     app.log.info(`server listening on ${address}`)
 }
 
